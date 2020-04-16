595544a850f2607d2e1fad9fbcad7c4cc12487a3
Alex Baker <baker.alex.j@gmail.com>
2020-04-16 11:31:28 -0600

    Revisions for TA02

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localho
 const app = express();
 
 // Route setup. You can implement more in the future!
-const ta01Routes = require('./routes/ta01'); // Not necessary for team activity 02
-const ta03Routes = require('./routes/ta03'); // Core requirement 03
-const ta04Routes = require('./routes/ta04'); // Stretch challenge 02
+const ta01Routes = require('./routes/ta01');
+const ta02Routes = require('./routes/ta02');
+const ta03Routes = require('./routes/ta03'); 
+const ta04Routes = require('./routes/ta04'); 
 
 app.use(express.static(path.join(__dirname, 'public')))
    .set('views', path.join(__dirname, 'views'))
@@ -31,15 +32,16 @@ app.use(express.static(path.join(__dirname, 'public')))
    // For view engine as hbs (Handlebars)
    //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
    //.set('view engine', 'hbs')
-   .use('/ta01', ta01Routes) // Not necessary for team activity 02
-   .use('/ta03', ta03Routes) // Core requirement 03
-   .use('/ta04', ta04Routes) // Stretch challenge 02
+   .use('/ta01', ta01Routes)
+   .use('/ta02', ta02Routes) 
+   .use('/ta03', ta03Routes) 
+   .use('/ta04', ta04Routes)
    .get('/', (req, res, next) => {
-     // This is the primary index
+     // This is the primary index, always handled last. 
      res.render('pages/index', {title: 'Welcome to my CSE341 repo', path: '/'});
     })
    .use((req, res, next) => {
-     // This will serve as our 404 page for stretch challenge 01
-     res.render('pages/404', {title: '404 - Page Not Found', path: req.url})
+     // 404 page
+     res.render('pages/404', {title: '404 - Page Not Found', path:''})
    })
    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
